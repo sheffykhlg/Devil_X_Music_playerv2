a1a9ee531940a486e4414375b380947740fe9e94
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-07 15:37:23 +0530

    Update mongo.py

--- devil/core/mongo.py
+++ devil/core/mongo.py
@@ -1,4 +1,4 @@
-# Powered By Team Devil Or misterjack18 IF You Fresh Any Problem To Contact The BgtRobot Owner
+# Powered By Team Devil Or misterjack18 IF You Fresh Any Problem To Contact The DevilXmusicbot Owner
 
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
@@ -8,7 +8,7 @@ from Bikash import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://bikashhalder123:bikash143@cluster0.u6kvanf.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://sahilsaim1919: sahilsaim1919@cluster0.u6kvanf.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
@@ -16,7 +16,7 @@ if config.MONGO_DB_URI is None:
         "?? ???? MONGO DB URL ??????????..? ???????? ?????? ???????? ???????? ???? @BikashHalder MONGO ???????????????? ????"
     )
     temp_client = Client(
-        "BikashX",
+        "Teamdevil",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -32,6 +32,6 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Bikashh
-    pymongodb = _mongo_sync_.Devil
+    mongodb = _mongo_async_.sahilsaim1919
+    pymongodb = _mongo_sync_.sahilsaim1919
 
