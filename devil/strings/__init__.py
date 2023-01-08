c114b91f30701ce1bed9e7766564c516c3fb2c1c
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-08 13:13:46 +0530

    Update and rename __init__.py.txt to __init__.py

--- devil/strings/__init__.py
+++ devil/strings/__init__.py
@@ -1,4 +1,4 @@
-# Powered By @BikashHalder @AdityaHalder
+# Powered By @Teams_devil @misterjack18
 
 import os
 from typing import List
@@ -17,27 +17,27 @@ def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./Bikash/strings"):
+for filename in os.listdir(r"./Devil/strings"):
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         commands[language_name] = yaml.safe_load(
-            open(r"./Bikash/strings/" + filename, encoding="utf8")
+            open(r"./Devil/strings/" + filename, encoding="utf8")
         )
 
 
-for filename in os.listdir(r"./Bikash/strings/langs/"):
+for filename in os.listdir(r"./Devil/strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./Bikash/strings/langs/en.yml", encoding="utf8")
+            open(r"./Devil/strings/langs/en.yml", encoding="utf8")
         )
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
-            open(r"./Bikash/strings/langs/" + filename, encoding="utf8")
+            open(r"./Devil/strings/langs/" + filename, encoding="utf8")
         )
         for item in languages["en"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
-Footer
+
