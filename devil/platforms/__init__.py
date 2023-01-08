622f43fa835fffbe6cebea06714c131d6e53d75a
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-08 13:09:23 +0530

    Update and rename __init__.py.txt to __init__.py

--- devil/platforms/__init__.py
+++ devil/platforms/__init__.py
@@ -1,4 +1,4 @@
-# Powered By BikashHalder Or Aditya Halder IF You Fresh Any Problem To Contact The BgtRobot Owner
+# Powered By Team Devil Or misterjack18 IF You Fresh Any Problem To Contact The DevilXmusicbot Owner
 
 from .Apple import AppleAPI
 from .Carbon import CarbonAPI
@@ -7,4 +7,3 @@ from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
 from .Youtube import YouTubeAPI
-Footer
