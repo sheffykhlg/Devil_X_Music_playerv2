423def7ec950c05f26f4a266168e7e4f242a0dd0
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-17 09:21:46 +0530

    Update and rename ping.py.txt to ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,19 +1,19 @@
-# Powered By @BikashHalder @AdityaHalder
-# ©? Copy Right By Bikash Halder Or Aditya Halder
-# Any Problem To Report @Bgt_Chat or @AdityaDiscus
-# Bot Owner @BikashHalder Or @AdityaHalder
+# Powered By @Teams_devil @misterjack18
+# Â©? Copy Right By Teams_devil Or misterjack18 
+# Any Problem To Report Teams_devil or @sabyahaapnehai
+# Bot Owner @Teams_devil Or @misterjack18
 
 from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Bikash.config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
-from Bikash.strings import get_command
-from Bikash import app
-from Bikash.core.call import Bikashh
-from Bikash.utils import bot_sys_stats
-from Bikash.utils.decorators.language import language
+from Devil.config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from Devil.strings import get_command
+from Devil import app
+from Devil.core.call import Devill
+from Devil.utils import bot_sys_stats
+from Devil.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
