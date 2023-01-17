e6b8dde83f6f63ff32a0c31152257b97162cea25
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-17 09:27:08 +0530

    Update and rename stop.py.txt to stop.py

--- plugins/stop.py
+++ plugins/stop.py
@@ -1,17 +1,17 @@
-# Powered By @BikashHalder @AdityaHalder
-# ©? Copy Right By Bikash Halder Or Aditya Halder
-# Any Problem To Report @Bgt_Chat or @AdityaDiscus
-# Bot Owner @BikashHalder Or @AdityaHalderfrom pyrogram import filters
+# Powered By @Teams_devil @misterjack
+# Â©? Copy Right By Teams_devil Or misterjack18 
+# Any Problem To Report @Teams_devil or @sabyahaapnehai
+# Bot Owner @Teams_devil Or @misterjack18 from pyrogram import filters
 
 from pyrogram.types import Message
 from pyrogram import filters
 
-from Bikash.config import BANNED_USERS
-from Bikash.strings import get_command
-from Bikash import app
-from Bikash.core.call import Bikashh
-from Bikash.utils.database import set_loop
-from Bikash.utils.decorators import AdminRightsCheck
+from Devil.config import BANNED_USERS
+from Devil.strings import get_command
+from Devil import app
+from Devil.core.call import Devill 
+from Devil.utils.database import set_loop
+from Devil.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -27,7 +27,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Bikash.stop_stream(chat_id)
+    await Devil.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
