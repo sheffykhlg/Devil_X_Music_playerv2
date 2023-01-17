97cf1e19f7d89983cfca2a2878f4ea6e45fac640
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-17 09:41:37 +0530

    Update and rename speedtest.py.txt to speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -1,7 +1,7 @@
-# Powered By @BikashHalder @AdityaHalder
-# ©? Copy Right By Bikash Halder Or Aditya Halder
-# Any Problem To Report @Bgt_Chat or @AdityaDiscus
-# Bot Owner @BikashHalder Or @AdityaHalder
+# Powered By @Teams_devil @misterjack18
+# Â©? Copy Right By  Teams_devil Or misterjack18 
+# Any Problem To Report @Teams_devil or @sabyahaapnehai
+# Bot Owner @Teams_devil Or @misterjack18
 
 
 import asyncio
@@ -11,9 +11,9 @@ import speedtest
 import wget
 from pyrogram import filters
 
-from Bikash.strings import get_command
-from Bikash import app
-from Bikash.misc import SUDOERS
+from Devil.strings import get_command
+from Devil import app
+from Devil.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
