5e35239052943fab1b128019677d03b8c07dd54f
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-23 14:57:17 +0530

    Update and rename shuffle.py.txt to shuffle.py

--- plugins/shuffle.py
+++ plugins/shuffle.py
@@ -1,18 +1,18 @@
-# Powered By @BikashHalder @AdityaHalder
-# ©? Copy Right By Bikash Halder Or Aditya Halder
-# Any Problem To Report @Bgt_Chat or @AdityaDiscus
-# Bot Owner @BikashHalder Or @AdityaHalder
+# Powered By @Teams_devil @misterjack18
+# Â©? Copy Right By Teams_devil Or misterjack18 
+# Any Problem To Report @Teams_devil or @sabyahaapnehai
+# Bot Owner @Teams_devil Or @misterjack18
 
 import random
 
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Bikash.config import BANNED_USERS
-from Bikash.strings import get_command
-from Bikash import app
-from Bikash.misc import db
-from Bikash.utils.decorators import AdminRightsCheck
+from Devil.config import BANNED_USERS
+from Devil.strings import get_command
+from Devil import app
+from Devil.misc import db
+from Devil.utils.decorators import AdminRightsCheck
 
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
