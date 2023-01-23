faa52ff7e516545c921145046e884d00c657d389
ᵀᴱᴬᴹ ᴰᴱᵛᴵᴸ <108905157+sahilsaim1919@users.noreply.github.com>
2023-01-23 22:03:15 +0530

    Delete devil directory

--- devil/platforms/__init__.py
+++
@@ -1,9 +0,0 @@
-# Powered By Team Devil Or misterjack18 IF You Fresh Any Problem To Contact The DevilXmusicbot Owner
-
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
--- devil/utils/inline/__init__.py
+++
@@ -1,9 +0,0 @@
-# Powered By @Teams_devil @misterjack18
-
-from .help import *
-from .play import *
-from .playlist import *
-from .queue import *
-from .settings import *
-from .song import *
-from .start import *
